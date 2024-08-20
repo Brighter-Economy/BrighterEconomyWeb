8fc5e3d75495ac3401d75ee6819f481c47309d80
jacobgelliss <jacobgelliss@gmail.com>
2024-08-20 15:48:02 -0500

    Added more pages and make a skeleton for the Item Market Page. Also added more of the Navigation tabs

---
+++ BEWebPortal/Pages/Compenents/ItemMarketTable.cs
@@ -0,0 +1,12 @@
+﻿namespace BEWebPortal;
+
+public struct ItemMarketTable
+{
+    public ItemMarketTable(String itemID, String itemName)
+    {
+        this.itemID = itemID;
+        this.itemName = itemName;
+    }
+    public String itemID { get; set; }
+    public String itemName { get; set; }
+}
